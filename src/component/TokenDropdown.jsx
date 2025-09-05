2ec0eb2ccfb14b3f843f774b7e17b9e8e7475b9f
faheem303 <faheemrehmat360@gmail.com>
2025-09-05 13:14:25 +0500

    desktop wallet updated

---
+++ src/component/TokenDropdown.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+export default function TokenSelectDropdown({ tokens, onChange }) {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(tokens[0]); // default first token
+
+  const handleSelect = (token) => {
+    setSelected(token);
+    setOpen(false);
+    onChange?.(token);
+  };
+
+  return (
+    <div className="relative w-[100%]">
+      {/* Selected button */}
+      <button
+        onClick={() => setOpen(!open)}
+        className="flex items-center justify-between w-full px-2 h-[32px]  rounded-[8px] cursor-pointer"
+        style={{ background: "rgba(255, 255, 255, 0.20)" }}
+      >
+        <span className="flex items-center gap-2 text-[11px] font-[700] font-[Inter]">
+          {selected.icon && (
+            <img
+              src={selected.icon}
+              alt={selected.symbol}
+              className="w-5 h-5"
+            />
+          )}
+          <span className="leading-[8px]">
+            {selected.symbol}
+            <br />
+            {selected.sub_symbol && (
+              <span className="text-[6px] font-[400]">
+                {selected.sub_symbol}
+              </span>
+            )}
+          </span>
+        </span>
+        <svg
+          className={`w-4 h-4 transition-transform ${open ? "rotate-180" : ""}`}
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+
+      {/* Dropdown list */}
+      {open && (
+        <div
+          className="absolute left-0 mt-1 w-full border border-gray-300 rounded-md shadow-md z-10 overflow-hidden"
+          style={{ background: "rgba(54, 54, 54, 0)",
+            backdropFilter: "blur(5px)",
+
+           }}
+        >
+          {tokens.map((token) => (
+            <button
+              key={token.id}
+              onClick={() => handleSelect(token)}
+              className="flex items-center gap-2 w-full px-3 py-2 text-[11px] font-[700] font-[Inter] text-left !hover:bg-gray-100/80 "
+              style={{
+                background:"rgba(201, 187, 187, 0.47)",
+              }}
+            >
+              <img src={token.icon} alt={token.symbol} className="w-5 h-5" />
+              {token.symbol}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
