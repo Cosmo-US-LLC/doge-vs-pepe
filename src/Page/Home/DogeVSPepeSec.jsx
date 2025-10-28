ef2e1333b84078d28c3b8600444165e89bbc7cc6
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-10-28 16:30:02 +0500

    5 section update

--- src/Page/Home/DogeVSPepeSec.jsx
+++ src/Page/Home/DogeVSPepeSec.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import bit from "../../assets/DogeVSPepeSec/bit.svg";
-import coin from "../../assets/DogeVSPepeSec/coin.svg";
-import tioicn from "../../assets/DogeVSPepeSec/crypto.svg";
+import card1 from "../../assets/DogeVSPepeSec/card1.png";
+import card2 from "../../assets/DogeVSPepeSec/card2.png";
+import card3 from "../../assets/DogeVSPepeSec/card3.png";
+import cryptoLogo from "../../assets/DogeVSPepeSec/v2_crypto_logo.webp";
+import coinSpeakerLogo from "../../assets/DogeVSPepeSec/v2_coinspeake_logo.webp";
+import bitcoinistLogo from "../../assets/DogeVSPepeSec/v2_bitcoin_logo.webp";
 import arw from "../../assets/DogeVSPepeSec/arw.svg";
 import topShadow from "../../assets/DogeVSPepeSec/top-shadow.webp";
 import bottomShadow from "../../assets/DogeVSPepeSec/bottom-shadow.webp";
-import cryShadow from "../../assets/DogeVSPepeSec/cry-shadow.png";
+import cryptoNewsLogo from "../../assets/DogeVSPepeSec/v2_dvp_logo.webp";
 
 function DogeVSPepeSec() {
   return (
-    <div className="relative w-[100%] h-[900px] flex justify-center items-center dogeVsPepeBG overflow-hidden">
+    <div className="relative w-[100%] h-[700px] flex justify-center items-center dogeVsPepeBG overflow-hidden">
       <div className="absolute top-0 left-0 right-0 w-full h-full">
         <img className="w-full h-full" src={topShadow} alt="" />
       </div>
@@ -18,88 +21,123 @@ function DogeVSPepeSec() {
       </div>
       <div className="max-w-[1038px] w-[100%] space-y-[50px] mx-auto relative z-10">
         <div className=" w-[100%]">
-          <h3 className="text-center section_heading">
-            DOGE VS PEPE IS MAKING HEADLINES
-          </h3>
+          <img
+            src={cryptoNewsLogo}
+            alt=""
+            className="w-full max-w-[760px] mx-auto h-full"
+          />
+          <h2 className="text-center">IS MAKING HEADLINES</h2>
         </div>
-        <div className="flex justify-center space-x-[30px]">
-          <div className="crypbg rounded-[15.016px] border min-h-[614.73px] max-w-[675.732px]  border-[#fff]">
-            <div className="py-[20px] px-[35px] bg-[#fff] rounded-t-[15.016px]">
-              <img src={tioicn} alt="" />
-            </div>
-            <div className="px-[35px] space-y-[20px] pt-[290px] pb-[20px] relative overflow-hidden">
-              <div className="relative z-10 space-y-0">
-                <h3 className="text-[42.703px] font-[400] text-[#fff] leading-normal  uppercase font-anton">
-                  <span className="text-[#FFBF01]">Doge</span> vs{" "}
-                  <span className="text-[#00FF2F]">Pepe ?</span>
+        <div className="flex justify-center space-x-[20px]">
+          {/* Card 1 - THE OG MEME IS BACK */}
+          <div className="relative w-[350px] h-[330px] rounded-[20px] overflow-hidden border-[4px] border-[#fff]">
+            {/* Background Image */}
+            <img
+              src={card1}
+              alt="Doge boxing"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+
+            {/* Text Overlay */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent flex flex-col justify-end">
+              <div className=" space-y-4">
+                <h3 className="p-6 text-[28px] font-bold text-white uppercase font-anton leading-tight">
+                  THE OG MEME IS BACK!
                 </h3>
 
-                <p className="w-[500.806px] h-[131.076px] rotate-[-0.081deg] flex-shrink-0 text-[#FFF] font-helvetica text-[16px] font-[400] leading-[22px]">
+                <p className="p-6 text-white font-helvetica text-[14px] leading-[20px]">
                   Doge or Pepe? Fans are rallying to decide who has the
                   strongest community. Will Doge stay on top, or is Pepe set to
                   dethrone him? The battle is on!
                 </p>
-              </div>
-              <div className="relative z-10 flex space-x-2">
-                <h4 className=" text-[16px] font-[400] leading-[32px] font-helvetica text-[#fff]">
-                  Find out more
-                </h4>
-                <img src={arw} alt="" className="mt-[5px]" />
-              </div>
-              <div className="absolute bottom-[0px] left-0 right-0">
-                <img className="rounded-b-[15.016px]" src={cryShadow} alt="" />
+
+                <div className="flex justify-between items-center bg-[#fff] h-[60px] p-6">
+                  <div className="flex items-center space-x-2">
+                    <img src={cryptoLogo} alt="" className="w-[140px] h-full" />
+                  </div>
+                  <div className="flex items-center text-[#000] text-[12px]">
+                    <span>Find out more</span>
+                    <img src={arw} alt="" className="w-5 h-5" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-between space-y-[20px]">
-            <div className="crypbg2 rounded-[15.016px] border min-h-[300.97px]  max-w-[675.732px]  border-[#fff]">
-              <div className="py-[10px] px-[15px] bg-[#fff] rounded-t-[15.016px]">
-                <img src={coin} alt="" />
-              </div>
-              <div className="px-[15px] pt-[126px] space-y-[10px]  pb-[20px]">
-                <div className="">
-                  <h3 className="text-[20px] leading-normal font-[400] text-[#fff] uppercase font-anton">
-                    <span className="text-[#FFBF01]">legendary</span>
-                    <span className="text-[#00FF2F]">memes face off</span>
-                  </h3>
 
-                  <p className="w-[305.664px] h-[67.998px] rotate-[-0.081deg] flex-shrink-0 text-[#FFF] font-helvetica text-[14px] font-[400] leading-[17px] pt-[10px]">
-                    Two legendary memes face off in an exciting presale, with
-                    rumors of well-known crypto developers behind the project.
-                    This could be the next big thing!
-                  </p>
-                </div>
-                <div className="flex pt-2 space-x-2">
-                  <h4 className="text-[16px] font-[400] leading-3 font-helvetica text-[#fff]">
-                    Find out more
-                  </h4>
-                  <img src={arw} alt="" />
+          {/* Card 2 - TWO LEGENDS FACE OFF */}
+          <div className="relative w-[350px] h-[330px] rounded-[20px] overflow-hidden border-[4px] border-[#fff]">
+            {/* Background Image */}
+            <img
+              src={card2}
+              alt="Doge vs Pepe boxing"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+
+            {/* Text Overlay */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-transparent flex flex-col justify-end">
+              <div className="space-y-4">
+                <h3 className=" p-6 text-[28px] font-bold text-white uppercase font-anton leading-tight">
+                  TWO LEGENDS FACE OFF
+                </h3>
+
+                <p className="p-6  text-white font-helvetica text-[14px] leading-[20px]">
+                  Finding the next winning narrative is the key to hitting big
+                  with memes. Two presales, but only one will win and launch!
+                  Backed by two of the most iconic meme coins, this might just
+                  be it.
+                </p>
+
+                <div className="flex justify-between items-center bg-[#fff] h-[60px] p-6">
+                  <div className="flex items-center space-x-2">
+                    <img
+                      src={coinSpeakerLogo}
+                      alt=""
+                      className="w-[140px] h-full"
+                    />
+                  </div>
+                  <div className="flex items-center space-x-1 text-[#000] text-[12px]">
+                    <span>Find out more</span>
+                    <img src={arw} alt="" className="w-5 h-5" />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="crypbg3 rounded-[15.016px] border min-h-[300.97px]  max-w-[675.732px]  border-[#fff]">
-              <div className="py-[10px] px-[15px] bg-[#fff] rounded-t-[15.016px]">
-                <img src={bit} alt="" />
-              </div>
-              <div className="px-[15px] pt-[112px] space-y-[10px]  pb-[20px]">
-                <div className="">
-                  <h3 className="text-[20.907px] leading-[100%] font-[400] text-[#fff] uppercase font-anton">
-                    <span className="text-[#FFBF01]">Who will</span>
-                    <span className="text-[#00FF2F]">be the Winner</span>
-                  </h3>
+          </div>
 
-                  <p className="w-[309.443px] h-[69.514px] rotate-[-0.081deg] flex-shrink-0 text-[#FFF] font-helvetica text-[14px] font-[400] leading-[18.592px] pt-[5px]">
-                    Finding the next winning narrative is the key to hitting big
-                    with memes. Two presales, but only one will win and launch!
-                    Backed by two of the most iconic meme coins, this might just
-                    be it.
-                  </p>
-                </div>
-                <div className="flex pt-2 space-x-2">
-                  <h4 className="text-[16px] font-[400] leading-[32px] font-helvetica text-[#fff]">
-                    Find out more
-                  </h4>
-                  <img src={arw} alt="" />
+          {/* Card 3 - THE NEW MEME KING */}
+          <div className="relative w-[350px] h-[330px] rounded-[20px] overflow-hidden border-[4px] border-[#fff]">
+            {/* Background Image */}
+            <img
+              src={card3}
+              alt="Pepe boxing"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+
+            {/* Text Overlay */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-transparent flex flex-col justify-end">
+              <div className=" space-y-4">
+                <h3 className=" p-6 text-[28px] font-bold text-white uppercase font-anton leading-tight">
+                  THE NEW MEME KING?
+                </h3>
+
+                <p className="p-6  text-white font-helvetica text-[14px] leading-[20px]">
+                  Two legendary memes face off in an exciting presale, with
+                  rumors of well-known crypto developers behind the project.
+                  This could be the next big thing!
+                </p>
+
+                <div className="flex justify-between items-center bg-[#fff] h-[60px] p-6">
+                  <div className="flex items-center space-x-2">
+                    <img
+                      src={bitcoinistLogo}
+                      alt=""
+                      className="w-[140px] h-full"
+                    />
+                  </div>
+                  <div className="flex items-center space-x-1 text-[#000] text-[12px]">
+                    <span>Find out more</span>
+                    <img src={arw} alt="" className="w-5 h-5" />
+                  </div>
                 </div>
               </div>
             </div>
